bd310daf295df29ff374be8d01b60b8c2fcf26f0
chodavarapu sowdhamini <94843433+sowdhamini-buildbot@users.noreply.github.com>
2022-03-08 17:28:37 +0530

    Add files via upload

---
+++ break.js
@@ -0,0 +1,8 @@
+//Loop
+for (var i = 0; i <= 10; i++){
+    if(i==5){
+        break
+    }else{
+        console.log(i);
+    }
+}
---
+++ dowhile.js
@@ -0,0 +1,12 @@
+var i = 5;
+do {
+    console.log("You got " + i)
+    i++;
+} while (i <= 10);
+
+//2nd
+var r = 5;
+do {
+    console.log("You have " + 5);
+    i++;
+} while (i <= 5);
